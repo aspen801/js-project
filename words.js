51f8db817e47768e4cf89ff0942030dffa163ed3
Bogdan Boyko <boyko.bogdan45@gmail.com>
2022-08-13 23:44:43 +0300

    added words.js

---
+++ words.js
@@ -0,0 +1,17 @@
+import _ from 'lodash';
+
+const countWords = (sentence) => {
+    const result = {};
+    
+    // var words = (sentence.toLowerCase()).split(' ');
+    var words = sentence.toLowerCase();
+    words = _.words(words);
+    
+    for(const word of words){
+        result[word] = (result[word] ?? 0) + 1;
+    }
+
+    return result;
+}
+
+export default countWords;
