171cd213184694bc696cd814a44a420097a78acd
Bogdan Boyko <boyko.bogdan45@gmail.com>
2022-08-14 14:58:36 +0300

    added testdata.js

---
+++ testdata.js
@@ -0,0 +1,13 @@
+export const lesson = {
+    name: 'ДеструКТУРИЗАЦИЯ',
+    description: 'каК удивитЬ колек',
+  };
+
+  export const company1 = { name: 'Hexlet', state: 'moderating', website: 'https://hexlet.io' };
+  export const company2 = { name: 'CodeBasics', state: 'published', website: 'https://code-basics.com' };
+
+  export const company3 = { name: 'Hexlet', state: 'published', website: 'https://hexlet.io' };
+  export const company4 = { name: 'Hexlet', state: 'published', website: 'https://hexlet.io' };
+
+  export var domain = 'http://github.com/aspen801/js-project/blob/main/domain.js';
+  export var loremText = 'lorem LOrEm  ipsum   forem aren    gobel    IpSuM   gObel     GobEl';
