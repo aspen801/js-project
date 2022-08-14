9ce520edf97acd208121994ceaa05a7bc639fe64
Bogdan Boyko <boyko.bogdan45@gmail.com>
2022-08-14 22:14:12 +0300

    added objects.js

--- objects.js
+++ objects.js
@@ -1,9 +1,15 @@
-const user = {
-  name: 'Ivan',
-  age: 32,
-  car: 'Chevrolet',
+import * as testData from './testdata.js';
+
+const getObject = (object, keyPath) => {
+  var tempObj = object;
+  for (const key of keyPath) {
+    if (Object.prototype.hasOwnProperty.call(tempObj, key)) {
+      tempObj = tempObj[key];
+    } else return null;
+  }
+  return tempObj;
 };
 
-const getObj = () => user;
+console.log(getObject(testData.testObject, ['hosts', 1, 'null']));
 
-export default getObj;
+export default getObject;
