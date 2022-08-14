22c60e78b226721e73da52b7a2a7c51d9f454182
Bogdan Boyko <boyko.bogdan45@gmail.com>
2022-08-14 14:59:00 +0300

    major update

--- index.js
+++ index.js
@@ -2,24 +2,17 @@ import normalize from './normalize.js';
 import compareObjects from './compare.js';
 import getDomainInfo from './domain.js';
 import countWords from './words.js';
+import * as testData from './testdata.js';
 
-const lesson = {
-  name: 'ДеструКТУРИЗАЦИЯ',
-  description: 'каК удивитЬ колек',
-};
 
-const company1 = { name: 'Hexlet', state: 'moderating', website: 'https://hexlet.io' };
-const company2 = { name: 'CodeBasics', state: 'published', website: 'https://code-basics.com' };
 
-const company3 = { name: 'Hexlet', state: 'published', website: 'https://hexlet.io' };
-const company4 = { name: 'Hexlet', state: 'published', website: 'https://hexlet.io' };
 
-console.log(compareObjects(company1, company2));
-console.log(compareObjects(company3, company4));
+console.log(compareObjects(testData.company1, testData.company2));
+console.log(compareObjects(testData.company3, testData.company4));
 
-normalize(lesson)
-console.log(lesson);
-console.log(getDomainInfo('http://github.com/aspen801/js-project/blob/main/domain.js'));
-console.log(countWords('lorem LOrEm  ipsum   forem aren    gobel    IpSuM   gObel     GobEl'))
+normalize(testData.lesson)
+console.log(testData.lesson);
+console.log(getDomainInfo(testData.domain));
+console.log(countWords(testData.loremText));
 
 
