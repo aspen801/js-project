c919437fee79c0b824fb4a2e300a8866fcbc2cc5
Bogdan Boyko <boyko.bogdan45@gmail.com>
2022-08-19 22:23:28 +0300

    added date.js

---
+++ functions/date.js
@@ -0,0 +1,14 @@
+const convert = (...dates) => {
+  const dateStrings = [];
+  let i = 0;
+
+  for (const date of dates) {
+    const d = new Date(date);
+    dateStrings[i] = d.toDateString();
+    i += 1;
+  }
+
+  return dateStrings;
+};
+
+console.log(convert([1993, 3, 24], [1997, 8, 12], [2001, 10, 18]));
