87900f1f5a1fe6891d57fe41f1c2f07751efd184
Bogdan Boyko <boyko.bogdan45@gmail.com>
2022-09-17 22:00:30 +0300

    added jest test for cart.js

---
+++ _tests_/cart.test.js
@@ -0,0 +1,18 @@
+import makeCart  from '../functions/cart.js'
+
+test('cart', () => {
+    const cart = makeCart();
+    cart.addItem({ name: 'car', price: 3 }, 5);
+    cart.addItem({ name: 'house', price: 10 }, 2);
+    expect(cart.items[0]).toStrictEqual({goodProperties: {name: 'car', price: 3}, count: 5});
+    expect(cart.items[1]).toStrictEqual({goodProperties: {name: 'house', price: 10}, count: 2});
+    expect(cart.items.length).toEqual(2);
+    expect(cart.getCount()).toEqual(7);
+    expect(cart.getCost()).toEqual(35);
+    cart.addItem({ name: 'house', price: 10 }, 2);
+    expect(cart.items[1]).toStrictEqual({goodProperties: {name: 'house', price: 10}, count: 4});
+    expect(cart.items.length).toEqual(2);
+    expect(cart.getCount()).toEqual(9);
+    expect(cart.getCost()).toEqual(55);  
+});
+
