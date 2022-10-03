6e97ccafd27e802bf6ad4347903c5b43838da393
Bogdan Boyko <boyko.bogdan45@gmail.com>
2022-10-03 13:35:16 +0300

    minor update

--- trees/downcase-file-names.js
+++ trees/downcase-file-names.js
@@ -4,18 +4,17 @@ const tree = mkdir('/', [
     mkdir('eTc', [
       mkdir('NgiNx'),
       mkdir('CONSUL', [
-        mkfile('config.json'),
+        mkfile('coNfig.json'),
       ]),
     ]),
     mkfile('hOsts'),
   ]);
 
 const downcaseFileNames = (tree) => {
-    console.log(tree);
     if(isFile(tree)){
         return mkfile(tree.name.toLowerCase());
     } else {
-    const newChildren = tree.children.map((child) => downcaseFileNames(child, tree.name))
+    const newChildren = tree.children.map(downcaseFileNames)
     return mkdir(tree.name, newChildren);
     }
 }
