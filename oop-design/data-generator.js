35364df0174f85220e24bdfa8b45f5d7b20fe85a
Bogdan Boyko <boyko.bogdan45@gmail.com>
2022-10-15 11:55:18 +0300

    minor update

--- oop-design/data-generator.js
+++ oop-design/data-generator.js
@@ -1,26 +1,25 @@
 import * as fs from 'fs';
 
-fs.readFile("./data.json", "utf8", (err, jsonString) => {
+fs.readFile('./data.json', 'utf8', (err, jsonString) => {
   if (err) {
-    console.log("File read failed:", err);
+    console.log('File read failed:', err);
     return;
   }
   const data = JSON.parse(jsonString);
-  let resultString = '';
+  let resultString = 'export const countries = [\n';
   for (const item in data) {
-    if(data[item].length < 20){
+    if (data[item].length < 20) {
       for (let i = 0; i < data[item].length; i++) {
-      resultString = resultString.concat(`{name: '${data[item][i]}', country: '${item}'},\n`);
+        resultString = resultString.concat(`    {name: '${data[item][i]}', country: '${item}'},\n`);
       }
     }
-    
   }
-  fs.writeFile('./country-city-list.txt', resultString, err => {
+  resultString = resultString.concat(']');
+  fs.writeFile('./country-city-list.js', resultString, (err) => {
     if (err) {
-        console.log('Error writing file', err)
+      console.log('Error writing file', err);
     } else {
-        console.log('Successfully wrote file')
+      console.log('Successfully wrote file');
     }
-})
+  });
 });
-
