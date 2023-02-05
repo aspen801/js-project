89b1965ac52635d19c4f784b5104408a53d753e0
Bogdan Boyko <boyko.bogdan45@gmail.com>
2023-02-05 12:23:56 +0200

    minor fix

--- dom/normalizeFunction.js
+++ dom/normalizeFunction.js
@@ -1,9 +1,6 @@
 function normalizeClasses(){
     const elements = document.body.getElementsByTagName('*');
     Array.from(elements).forEach(element => {
-        <body onload="normalizeClasses()">
-        <div class="text-center row-b">Bam</div>
-    </body>
            element.classList.forEach(className => {
             const newClassName = _.camelCase(className);
             element.classList.replace(className, newClassName);
