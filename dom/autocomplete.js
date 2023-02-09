8d7a58363abefd85436e1d5913f97c10af2270ed
konnichiwa991 <boyko.bogdan45@gmail.com>
2023-02-09 21:35:09 +0200

    added fetch

--- dom/autocomplete.js
+++ dom/autocomplete.js
@@ -1,6 +1,22 @@
 function inputListener() {
     const input = document.querySelector('.autocomplete');
-    input.addEventListener('input', () => {
-        console.log(input.value);
+    const ul = document.createElement('ul');
+    
+    document.body.append(ul);
+    input.addEventListener('input', (e) => {
+      const div = document.createElement('div');
+        fetch('https://restcountries.com/v3.1/all')
+      .then(response => response.json())
+      .then(json => {
+        json.forEach(element => {
+            if(element.name.common.startsWith(e.target.value) && e.target.value !== '') {
+                const country = document.createElement('li');
+                country.textContent = element.name.common;
+                div.append(country);
+            }
+            
+        });
+        ul.innerHTML = div.innerHTML;
+      })
     })
 }
